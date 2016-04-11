a3711353797318f16d8b252906168b287bc3fb6d
Shazia Zaman <shazia.a.zaman@gmail.com>
2016-04-11 09:21:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports = function render (locals, callback) {
+    callback(null, 
+    '<!DOCTYPE html><html><head><link rel="stylesheet" href="/style.css"></head><body>Hello World</body></html>')
+}
